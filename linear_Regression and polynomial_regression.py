78c7fd4efa988f2c007b6ce7e3e5c7ccbb5babdd
Amulya R <amulyar122006@gmail.com>
2026-09-23 22:27:34 +0530

    Update linear_Regression and polynomial_regression.py

--- linear_Regression and polynomial_regression.py
+++ linear_Regression and polynomial_regression.py
@@ -7,8 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
-
 # 2. LOAD DATASET
 data = pd.read_csv("Salary_Data.csv")
 
